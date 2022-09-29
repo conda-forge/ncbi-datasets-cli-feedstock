e07a8d88eadb0b0244438798415707021398036f
Brad Holmes <holmesbr@ncbi.nlm.nih.gov>
2022-09-29 17:22:28 -0400

    Build conda package from github assets

--- recipe/build.py
+++ recipe/build.py
@@ -1,59 +1,50 @@
-import json
 import os
-import shutil
-import subprocess
+import platform
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
 import sys
-import urllib.parse as urllib_parse
-import urllib.request as urllib_request
 
 
-def url_to_path(url):
-    """
-    Convert a file: URL to a path.
-    """
-    assert url.startswith('file:'), (
-        "You can only turn file: urls into filenames (not %r)" % url)
+def _determine_filename():
+   print(f"Platform: {platform.uname()}")
 
-    _, netloc, path, _, _ = urllib_parse.urlsplit(url)
+   if platform.uname().system == "Darwin":
+      return "darwin-universal.cli.package.zip"  
+   if platform.uname().system == "Windows":
+      return "windows-amd64.cli.package.zip"
+   return "linux-amd64.cli.package.zip"
 
-    # if we have a UNC path, prepend UNC share notation
-    if netloc:
-        netloc = '\\\\' + netloc
+def _files(zipfile):
+   if zipfile.startswith("windows"):
+       return ["dataformat.exe", "datasets.exe"]
+   return ["dataformat", "datasets"]
 
-    path = urllib_request.url2pathname(netloc + path)
-    return path
-
-
-def copy_output_files(build_event_file, bin_dir):
-    with open(build_event_file, 'r') as f:
-        for line in f.readlines():
-            event = json.loads(line) 
-            if 'completed' not in event:
-                continue
-            if 'importantOutput' in event['completed']:
-                for output_name in event['completed']['importantOutput']:
-                    path = url_to_path(output_name['uri'])
-                    outfile_name = os.path.abspath(path)
-                    target_name = os.path.join(bin_dir, os.path.basename(outfile_name))
-                    shutil.copy2(outfile_name, target_name)
-                    
 
 # Setup
 PREFIX=os.environ['PREFIX']
-os.chdir('pkgs/ncbi-datasets-cli')
 
 BIN_DIR='{}/bin/'.format(PREFIX)
 if not os.path.exists(BIN_DIR):
     os.mkdir(BIN_DIR)
 
-# Build executables
-subprocess.run('bazel build --build_event_json_file=build_events.json //src:datasets //src:dataformat',
-               shell=True, stdout=sys.stdout, stderr=sys.stderr)
+if len(sys.argv) != 2:
+   raise Exception("Must pass one argument, the version string, to this script")
+version = sys.argv[1]
+filename = _determine_filename()
 
-# Find and copy executables
-copy_output_files('build_events.json', BIN_DIR)
+url = f"https://github.com/ncbi/datasets/releases/download/v{version}/{filename}"
+print(f"URL: {url}")
 
-# Cleanup
-subprocess.run('bazel clean', shell=True, stdout=sys.stdout, stderr=sys.stderr)
-subprocess.run('bazel shutdown', shell=True, stdout=sys.stdout, stderr=sys.stderr)
+resp = urlopen(url)
+myzip = ZipFile(BytesIO(resp.read()))
+
+
+for file in _files(filename):
+   output = f"{BIN_DIR}/{file}"
+   print(f"Writing {output}")
+   with open(output, "wb") as fh:
+      for line in myzip.open(file).readlines():
+         fh.write(line)
+   os.chmod(output, 0o755)
 
